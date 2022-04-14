a9dec15a375e67a01f7f45ae7c846f78a2693a95
Ryan Remaly <remalyryan@gmail.com>
2022-04-14 08:45:00 -0400

    Adding quote

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ Will be using this repository to document the building of my geothermal greenhou
 
 Original inspiration for this project was Russ Finch. Taking most of my design choices from the people at Greencube Research. This technology is something that doesn't have a ton of exposure and I have to see if it's "too good to be true".
 
+{
+
+"Unlike solar, wind and most geothermal, the "geo air" system can be used in virtually any climate, anywhere in the world without a large investment, and without governmental regulation. Additionally, the "geo air" system can be easily installed by any licensed building contractor, or brave DIY consumer with a little skill.
+This Report is not just inspiring but it gives the reader all the information needed, from a real person, who has already been using it successfully for decades.
+This report and new technology is not specifically to promote geo-air use for a greenhouse. It will illustrate three (3) important designs that can be used to harness and utilize geothermal, geo-air energy for many applications. This is beneficial to many types of structures and uses.
+This new "geo air" technology could easily become the most profitable new technology, now, and for many years to come. The variations and applications are endless.
+The contents will allow the reader to adapt the “geo-air” technology to almost any design greenhouse, home, office, or warehouse." - Greencube Research
+
+
+}
+
  Design
 ------------
 
---
+++ xlxsWriter/materials_costs_sources.py
@@ -0,0 +1,98 @@
+import xlsxwriter
+
+# Create a workbook and add a thermalBatteryWorksheet.
+workbook = xlsxwriter.Workbook('Greenhouse/excelSheets/Material_Cost_Source.xlsx')
+thermalBatteryWorksheet = workbook.add_worksheet()
+
+# Add a bold format to use to highlight cells.
+bold = workbook.add_format({'bold': True})
+
+# Add a number format for cells with money.
+money = workbook.add_format({'num_format': '$#,##0'})
+
+# mm to foot conversion
+mmFt = 304.8
+
+# Write some data headers.
+thermalBatteryWorksheet.write('A1', 'Material', bold)
+thermalBatteryWorksheet.write('B1', 'Length (Feet)', bold)
+thermalBatteryWorksheet.write('C1', 'Width (Feet)', bold)
+thermalBatteryWorksheet.write('D1', 'Height (Feet)', bold)
+thermalBatteryWorksheet.write('E1', 'Length (mm)', bold)
+thermalBatteryWorksheet.write('F1', 'Width (mm)', bold)
+thermalBatteryWorksheet.write('G1', 'Height (mm)', bold)
+thermalBatteryWorksheet.write('H1', 'Cost', bold)
+thermalBatteryWorksheet.write('I1', 'Quantity', bold)
+thermalBatteryWorksheet.write('J1', 'Source', bold)
+
+# Some data we want to write to the thermalBatteryWorksheet.
+materials = (
+	['EPS Foam', 2438.4, 1219.2, 152.4, 12.90, 68, 'google.com'],
+)
+
+# Start from the first cell below the headers.
+row = 1
+col = 0
+
+# Iterate over the data and write it out row by row.
+for material, lengthMm, widthMm, heightMm, cost, quantity, source in (materials):
+    thermalBatteryWorksheet.write(row, col,     material)
+    thermalBatteryWorksheet.write(row, col + 1, lengthMm / mmFt)
+    thermalBatteryWorksheet.write(row, col + 2, widthMm / mmFt)
+    thermalBatteryWorksheet.write(row, col + 3, heightMm / mmFt )
+    thermalBatteryWorksheet.write(row, col + 4, lengthMm)
+    thermalBatteryWorksheet.write(row, col + 5, widthMm)
+    thermalBatteryWorksheet.write(row, col + 6, heightMm)
+    thermalBatteryWorksheet.write(row, col + 7, cost)
+    thermalBatteryWorksheet.write(row, col + 8, quantity)
+    thermalBatteryWorksheet.write(row, col + 9, source)
+    row += 1
+
+
+# Write a total using a formula.
+thermalBatteryWorksheet.write(row, 0, 'Total',       bold)
+thermalBatteryWorksheet.write(row, 1, '=SUM(B2:B5)', money)
+
+
+greenhouseWorksheet = workbook.add_worksheet()
+
+# Write some data headers.
+greenhouseWorksheet.write('A1', 'Material', bold)
+greenhouseWorksheet.write('B1', 'Length (Feet)', bold)
+greenhouseWorksheet.write('C1', 'Width (Feet)', bold)
+greenhouseWorksheet.write('D1', 'Height (Feet)', bold)
+greenhouseWorksheet.write('E1', 'Length (mm)', bold)
+greenhouseWorksheet.write('F1', 'Width (mm)', bold)
+greenhouseWorksheet.write('G1', 'Height (mm)', bold)
+greenhouseWorksheet.write('H1', 'Cost', bold)
+greenhouseWorksheet.write('I1', 'Quantity', bold)
+greenhouseWorksheet.write('J1', 'Source', bold)
+
+# Some data we want to write to the greenhouseWorksheet.
+materials = (
+	['EPS Foam', 2438.4, 609.6, 152.4, 1, 10, 'google.com'],
+	['EPS Foam', 2438.4, 1219.2, 50.8, 1, 90, 'google.com'],
+	['EPS Foam', 2438.4, 660.4, 25.4, 1, 30, 'google.com'],
+)
+
+# Start from the first cell below the headers.
+row = 1
+col = 0
+
+# Iterate over the data and write it out row by row.
+for material, lengthMm, widthMm, heightMm, cost, quantity, source in (materials):
+    greenhouseWorksheet.write(row, col,     material)
+    greenhouseWorksheet.write(row, col + 1, lengthMm / mmFt)
+    greenhouseWorksheet.write(row, col + 2, widthMm / mmFt)
+    greenhouseWorksheet.write(row, col + 3, heightMm / mmFt )
+    greenhouseWorksheet.write(row, col + 4, lengthMm)
+    greenhouseWorksheet.write(row, col + 5, widthMm)
+    greenhouseWorksheet.write(row, col + 6, heightMm)
+    greenhouseWorksheet.write(row, col + 7, cost)
+    greenhouseWorksheet.write(row, col + 8, quantity)
+    greenhouseWorksheet.write(row, col + 9, source)
+    row += 1
+
+
+
+workbook.close()
